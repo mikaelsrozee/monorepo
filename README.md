5a870ad68d1d54e9c1a7d60bc4273f57d3eaf737
Mikael Rozee <mikaelsrozee@gmail.com>
2020-09-21 22:00:51 +0100

    learn how to commit a whole repo properly

---
+++ README.md
@@ -0,0 +1,3 @@
+# Monorepo proof of concept
+
+Based off of [this blog post](https://josephluck.co.uk/blog/next-typescript-monorepo/)
---
+++ base/pages/index.jsx
@@ -0,0 +1,10 @@
+import Header from '../components/Header'
+
+function HomePage() {
+  return <div>
+    <Header />
+    <p>Content</p>
+  </div>
+}
+
+export default HomePage

d9e9daba986f5cbea1cdf9a7bddb3254c997d79d
Mikael Rozee <mikaelsrozee@gmail.com>
2020-09-21 21:59:55 +0100

    initial commit

---
+++ siteuno/next.config.js
@@ -0,0 +1,3 @@
+const withTranspileModules = require('next-transpile-modules')(['@mono'])
+
+module.exports = withTranspileModules()
---
+++ siteuno/pages/index.jsx
@@ -0,0 +1,10 @@
+import Header from '@mono/base/components/Header'
+
+function HomePage() {
+  return <div>
+    <Header />
+    <p>Lorem ipsum dolor sit amet.</p>
+  </div>
+}
+
+export default HomePage
